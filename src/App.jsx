020495b05616cb17f4d0b5243b76e8e7092cee2c
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-04-12 19:44:21 -0500

    add second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      hola mundo
-    </div>
-  );
+  return <div>hola mundo</div>;
 };
 
 export default App;
